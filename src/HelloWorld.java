f8e9484c341fe0d813626310919f59b5c1d10a9a
SUNG IL LEE <sungil.lee90@gmail.com>
2020-04-30 16:45:41 -0500

    feat:Premitive data type.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main (String arg[]) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Sung!");
+
+        int carPrice = 90000;
+        System.out.println(carPrice);
     }
 }
