cdcea21defe7ea45d44ce476b717bf137d3a5734
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-04 11:42:17 -0500

    Complete First Exercise Problem (FizzBuzz).

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,49 @@
 public class ControlFlowExercises {
     public static void main (String arg[]){
+        // Loop Basic
+
+        // While loop
         
-        int i = 5;
-        //i is less than or equal to 15
-        while (i <= 15) {
-            System.out.println("i = " + i);
-            i++;
+//        int i = 5;
+//        //i is less than or equal to 15
+//        while (i <= 15) {
+//            System.out.println("i = " + i);
+//            i++;
+//        }
+
+        // Do While loop
+//            int counter = 100;
+//            do {
+//                if (counter % 5 == 0) {
+//                System.out.println("counter = " + counter);
+//                }
+//                counter--;
+//            } while (counter >= -10);
+//        int counter = 2;
+//        do {
+//            if (counter % 2 == 0) {
+//                System.out.println("counter = " + counter * counter);
+//            }
+//            counter++;
+//        } while (counter < 1000000);
+
+//        // For loop
+//        for (int i = 5; i < 16; i++) {
+//            System.out.println("i = " + i);
+//        }
+        // Fizzbuzz
+        for (int i = 1; i <= 100; i ++) {
+            if (i % 3 == 0 && i % 5 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if( i % 5 == 0) {
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
         }
-//        boolean lessThanEqual = i <= 15;
-//        System.out.println("lessThanEqual = " + lessThanEqual);
-        
+
+
     }
 }
