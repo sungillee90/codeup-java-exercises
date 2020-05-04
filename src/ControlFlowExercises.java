f0925ca4afbb5a39a347c7bb72914f7016db369e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-04 16:42:03 -0500

    Complete Control Flow Exercise.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,41 +1,60 @@
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-    public static void main (String arg[]){
+    public static void main (String arg[]) {
         // Loop Basic
 
         // While loop
-        
+
 //        int i = 5;
 //        //i is less than or equal to 15
 //        while (i <= 15) {
-//            System.out.println("i = " + i);
+//            System.out.print(i + " ");
 //            i++;
 //        }
 
         // Do While loop
+
+//        int counter = 0;
+//        do {
+//            System.out.println("counter = " + counter);
+//            counter += 2;
+//        } while (counter <= 100);
+//
 //            int counter = 100;
 //            do {
-//                if (counter % 5 == 0) {
-//                System.out.println("counter = " + counter);
-//                }
-//                counter--;
+//                System.out.println(counter);
+//                counter -= 5;
 //            } while (counter >= -10);
-//        int counter = 2;
+
+    //                if (counter % 5 == 0) {
+    //                System.out.println("counter = " + counter);
+    //                }
+    //                counter--;
+    //            } while (counter >= -10);
+
+//        long counter = 2L;
 //        do {
-//            if (counter % 2 == 0) {
-//                System.out.println("counter = " + counter * counter);
-//            }
-//            counter++;
+//            System.out.println(counter);
+//            counter *= counter;
+////            if (counter % 2 == 0) {
+////                System.out.println("counter = " + counter * counter);
+////            }
+////            counter++;
 //        } while (counter < 1000000);
 
-//        // For loop
-//        for (int i = 5; i < 16; i++) {
+        // For loop
+//        for (int i = 100; i >= -10; i-= 5) {
 //            System.out.println("i = " + i);
 //        }
+        
+//        for (long counter = 2L; counter < 1000000L; counter *= counter){
+//            System.out.println("counter = " + counter);
+//        }
+        
         // Fizzbuzz
-//        for (int i = 1; i <= 100; i ++) {
-//            if (i % 3 == 0 && i % 5 ==0) {
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 ==0) { // or ( i % 15 == 0 )
 //                System.out.println("FizzBuzz");
 //            } else if (i % 3 == 0) {
 //                System.out.println("Fizz");
@@ -47,28 +66,81 @@ public class ControlFlowExercises {
 //        }
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter an integer that you would like to go up to: ");
-        int userInput = Integer.parseInt(sc.next());
-        System.out.println("userInput = " + userInput);
+//        boolean userContinues = true;
+//        do {
+//            System.out.println("Enter an integer that you would like to go up to: ");
+//            int userInput = Integer.parseInt(sc.next());
+//            System.out.println();
+//            System.out.println("Here is you table!");
+//            System.out.println();
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | --------| -----");
+//            for(int i = 1; i <= userInput; i++) {
+//                System.out.format("%-6d", i);
+//                System.out.print(" | ");
+//                System.out.format("%-7d",i * i); // suqared
+//                System.out.print(" | ");
+//                System.out.format("%-6d",i * i * i);
+//                System.out.println();
+//            }
+//            System.out.println("Do you want to continue? [y/n]");
+//            String userResponse = sc.next();
+//            if (!userResponse.equalsIgnoreCase("y")) {
+//                userContinues = false;
+//            }
+//
+//        } while (userContinues);
+            //        System.out.println("Enter an integer that you would like to go up to: ");
+            //        int userInput = Integer.parseInt(sc.next()); // scanner.nextInt() ALSO WORKS
+            //        System.out.println("userInput = " + userInput);
+            //        System.out.println();
+            //        System.out.println("Here is you table!");
 
-        System.out.println("Here is you table!");
+            //        System.out.println("Do you want to continue? [y/n]");
+            //        String userInputContinue = sc.next();
+                    // if statement and run the table.
+            //        boolean confirmation = userInputContinue.equals("y");
+            //        System.out.println("confirmation = " + confirmation);
 
-        System.out.println("Do you want to continue? [y/n]");
-        String userInputContinue = sc.next();
-        // if statement and run the table.
-        boolean confirmation = userInputContinue.equals("y");
-        System.out.println("confirmation = " + confirmation);
+            //        int number;
+            //        int square;
+            //        int cube;
+            //        System.out.println("Number\tSquare\tCube");
+            //
+            //        for (number = 1; number <= userInput; number++) {
+            //            square = number * number;
+            //            cube = number * square;
+            //            System.out.println(number+"\t"+square+"\t"+cube);
+            //        }
 
-        int number;
-        int square;
-        int cube;
-        System.out.println("Number\tSquare\tCube");
+        // 4. Convert given number grades into letter grades.
 
-        for (number = 1; number <= userInput; number++) {
-            square = number * number;
-            cube = number * square;
-            System.out.println(number+"\t"+square+"\t"+cube);
-        }
+        boolean anotherGrade = true;
+
+        do {
+            System.out.print("Please enter a numerical Grade from 0 to 100 ");
+
+            int numericGrade = sc.nextInt();
+
+            if (numericGrade >= 88) {
+                System.out.println("A");
+            } else if (numericGrade >= 80) {
+                System.out.println("B");
+            } else if (numericGrade >= 67) {
+                System.out.println("C");
+            } else if (numericGrade >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
+
+            System.out.println("Do you wish to enter a new grade? ");
+            String userResponse = sc.next();
+            if (!userResponse.equalsIgnoreCase("y")){
+                anotherGrade = false;
+            }
+        } while (anotherGrade);
+
 
     }
 }
