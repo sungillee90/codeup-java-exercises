3907810484466615220e1a0af03eaf468cd0afeb
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-04 11:03:01 -0500

    Complete First Exercise Problem (while loop).

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+public class ControlFlowExercises {
+    public static void main (String arg[]){
+        
+        int i = 5;
+        //i is less than or equal to 15
+        while (i <= 15) {
+            System.out.println("i = " + i);
+            i++;
+        }
+//        boolean lessThanEqual = i <= 15;
+//        System.out.println("lessThanEqual = " + lessThanEqual);
+        
+    }
+}
