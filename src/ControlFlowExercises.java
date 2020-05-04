d9e184bfb005c811689db4545818a970828b68ce
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-04 14:05:30 -0500

    Complete the Table of Powers.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main (String arg[]){
         // Loop Basic
@@ -32,18 +34,41 @@ public class ControlFlowExercises {
 //            System.out.println("i = " + i);
 //        }
         // Fizzbuzz
-        for (int i = 1; i <= 100; i ++) {
-            if (i % 3 == 0 && i % 5 ==0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if( i % 5 == 0) {
-                System.out.println("Buzz");
-            } else{
-                System.out.println(i);
-            }
-        }
+//        for (int i = 1; i <= 100; i ++) {
+//            if (i % 3 == 0 && i % 5 ==0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if( i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else{
+//                System.out.println(i);
+//            }
+//        }
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter an integer that you would like to go up to: ");
+        int userInput = Integer.parseInt(sc.next());
+        System.out.println("userInput = " + userInput);
+
+        System.out.println("Here is you table!");
+
+        System.out.println("Do you want to continue? [y/n]");
+        String userInputContinue = sc.next();
+        // if statement and run the table.
+        boolean confirmation = userInputContinue.equals("y");
+        System.out.println("confirmation = " + confirmation);
+
+        int number;
+        int square;
+        int cube;
+        System.out.println("Number\tSquare\tCube");
+
+        for (number = 1; number <= userInput; number++) {
+            square = number * number;
+            cube = number * square;
+            System.out.println(number+"\t"+square+"\t"+cube);
+        }
 
     }
 }
