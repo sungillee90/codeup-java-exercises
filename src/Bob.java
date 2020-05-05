c08dacd096b37d883c2bbe77dba655bd4ed52db7
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-05 11:50:14 -0500

    Complete String Exercises (StringExercise and Bob).

--- src/Bob.java
+++ src/Bob.java
@@ -2,21 +2,36 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main (String[] arg) {
-        System.out.println("heee");
+
         Scanner sc = new Scanner(System.in);
 
-        String askFirstQ = "Do you want to go to Osaka for dinner.";
-//        String askSecondQ = "Put it in the 4th gear!";
-        System.out.println("askFirstQ = " + askFirstQ);
-        if (askFirstQ.endsWith("?")) {
-            System.out.println("sure");
-        } else if (askFirstQ.endsWith("!")) {
+        System.out.println("What will you say to Bob? ");
+
+        String userResponse = sc.nextLine().trim();
+        System.out.println(userResponse);
+
+        if (userResponse.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (userResponse.endsWith("!")) {
             System.out.println("Whoa, chill out!");
-        } else if (askFirstQ.endsWith(" ")){
+        } else if (userResponse.equals("")){
             System.out.println("Fine. Be that way!");
         } else {
             System.out.println("Whatever");
         }
+
+//        String askFirstQ = "Do you want to go to Osaka for dinner.";
+////        String askSecondQ = "Put it in the 4th gear!";
+//        System.out.println("askFirstQ = " + askFirstQ);
+//        if (userResponse.endsWith("?")) {
+//            System.out.println("sure");
+//        } else if (userResponse.endsWith("!")) {
+//            System.out.println("Whoa, chill out!");
+//        } else if (userResponse.endsWith(" ")){
+//            System.out.println("Fine. Be that way!");
+//        } else {
+//            System.out.println("Whatever");
+//        }
 
 
     }
