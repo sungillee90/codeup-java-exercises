387760ac04fba32f51aa9416ff332fcfc88268c4
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-08 11:57:00 -0500

    Complete Third Exercise.

--- src/util/Input.java
+++ src/util/Input.java
@@ -6,15 +6,50 @@ import java.util.Scanner;
 // create a class named Input that has a private property named scanner.
 public class Input {
     // Creating fields (properties)
-//    private Scanner scanner = new Scanner(System.in);
-    private Scanner scanner;
+    private Scanner scanner = new Scanner(System.in);
 
-    private class Scanner {
-
+    public String getString() {
+        System.out.println("Type some message: ");
+        return scanner.nextLine();
     }
-    public static void main(String[] args) {
-        // Creating instances
 
+    // Do you want to continue? Yes or No 할수있다. 이거 들고가서
+    public boolean yesNo() {
+        System.out.println("Select [y/n]");
+        String answer = scanner.nextLine();
+        return (answer.equals("y") ? true : false);
+    }
+
+    public int getInt(int min, int max) {
+        System.out.println("Give me a number between " + min + " and " + max);
+        int userNumber = Integer.parseInt(scanner.nextLine()); // do NOT use nextInt();
+        if (userNumber < min || userNumber > max) {
+            // recursion or use FOR Loop to give user opportunity
+            System.out.println("Please try again keep in the range");
+            return getInt(min, max);
+        }
+        return userNumber;
+    }
+
+    public int getInt() {
+        System.out.println("Give me an integer");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public double getDouble(double min, double max) {
+        System.out.println("Give me a decimal between " + min + " and " + max);
+        double userNumber = Double.parseDouble(scanner.nextLine()); // do NOT use nextInt();
+        if (userNumber < min || userNumber > max) {
+            // recursion or use FOR Loop to give user opportunity
+            System.out.println("Please try again keep in the range");
+            return getDouble(min, max);
+        }
+        return userNumber;
+    }
+
+    public double getDouble() {
+        System.out.println("Give me an decimal: ");
+        return Double.parseDouble(scanner.nextLine());
     }
 
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,13 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+
+//        System.out.println("You typed: " + input.getString());
+//        System.out.println("Yes or No: " + input.yesNo());
+//        System.out.println(input.getInt());
+        System.out.println(input.getDouble());
+        System.out.println(input.getDouble(5.5,10.90));
+    }
+}
